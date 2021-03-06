e20021ad75111b2de284efb47e05e5e160733494
Thang <viktor.thang.phan@gmail.com>
2021-03-06 17:13:04 -0500

    Learned about gitignor

--- src/models/user/User.model.js
+++ src/models/user/User.model.js
@@ -4,7 +4,10 @@ const insertUser = (userObj) => {
   return new Promise((resolve, reject) => {
     UserSchema(userObj)
       .save()
-      .then((data) => {resolve(data);console.log("user created")})
+      .then((data) => {
+        resolve(data);
+        console.log("user created");
+      })
       .catch((err) => reject(err));
   });
 };
